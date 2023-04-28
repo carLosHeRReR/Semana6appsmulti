849a5f6af469bbcf7b219a67a172ed1756bacf2e
pachasp <elovfdhfshx475@gmail.com>
2023-04-27 19:32:30 -0500

    Agregar archivos de proyecto.

---
+++ ListViewExample/ListViewExample/ListViewGroup.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGroup : ContentPage
+    {
+        public ObservableCollection<Group> Groups { get; set; }
+        public ListViewGroup()
+        {
+            InitializeComponent();
+            Groups = new ObservableCollection<Group>
+        {
+            new Group("A")
+            {
+                new Item { Title = "Amelia", Subtitle = "Cedar" },
+                new Item { Title = "Alfie", Subtitle = "Spruce" },
+                new Item { Title = "Ava", Subtitle = "Pline" },
+                new Item { Title = "Archie", Subtitle = "Maple" }
+            },
+            new Group("B")
+            {
+                new Item { Title = "Brooke", Subtitle = "Lumia" },
+                new Item { Title = "Bobby", Subtitle = "Xperia" },
+                new Item { Title = "Bella", Subtitle = "Desire" },
+                new Item { Title = "Ben", Subtitle = "Chocolate" }
+            },
+            new Group("C")
+            {
+                new Item { Title = "Clhoe", Subtitle = "Brighton" },
+                new Item { Title = "Charlotte", Subtitle = "Hennietta" },
+                new Item { Title = "Mendez", Subtitle = "Diego" },
+                new Item { Title = "Connor", Subtitle = "Dallas" }
+            },
+        };
+            GroupedView.ItemsSource = Groups;
+        }
+    }
+}
---
+++ ListViewExample/ListViewExample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item15.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewGroup());
+            };
+
+        }
+    }
+}
